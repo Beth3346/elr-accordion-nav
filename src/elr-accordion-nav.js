5f0656b1b47b49a1e0df569fc6c0978e6bc7d27c
Elizabeth Rogers <beth3346@gmail.com>
2016-11-29 10:07:29 -0600

    use css transitions instead of jQuery animations

--- src/elr-accordion-nav.js
+++ src/elr-accordion-nav.js
@@ -1,129 +1,93 @@
 const $ = require('jquery');
 
-const elrAccordionNav = function(params) {
-    const self = {};
-    const spec = params || {};
-    const speed = spec.speed || 300;
-    const containerClass = spec.containerClass || 'elr-accordion-nav';
-    const expandIconClass = spec.expandIconClass  || 'fa-plus';
-    const collapseIconClass = spec.collapseIconClass  || 'fa-minus';
-    const iconClass = spec.iconClass || 'elr-accordion-icon';
+const elrAccordionNav = function({
+    containerClass = 'elr-accordion-nav',
+    labelClass = 'elr-accordion-nav-label'
+} = {}) {
     const contentHolderClass = contentHolderClass || 'elr-accordion-nav-inner';
     const $container = $(`.${containerClass}`);
+    const self = {
+        toggle($openContent, $openLabel) {
+            const $that = $(this);
+            const $nextContent = $that.next();
 
-    const showDefaultContent = function($expandedContent, $content) {
-        $content.hide();
-        $expandedContent.show();
-    };
-
-    const toggle = function(speed, $openContent) {
-        const $that = $(this);
-        const $nextContent = $that.next();
-
-            $openContent.slideUp(speed);
-
-            if ($nextContent.is(':hidden')) {
-                $nextContent.slideDown(speed);
-            } else {
-                $nextContent.slideUp(speed);
+            if (!$nextContent.hasClass('active')) {
+                $that.addClass('active');
+                $nextContent.addClass('active');
             }
+
+            $openLabel.removeClass('active');
+            $openContent.removeClass('active');
+        }
     };
 
-    const replaceIcons = function($openContent, iconClass, expandIconClass, collapseIconClass) {
-        const $that = $(this);
-        const $icon = $that.find(`.${iconClass}`);
-        const $openContentIcons = $openContent.prev().find(`.${iconClass}`);
+    const getCurrentPage = function(location, hash) {
+        const startIndex = location.lastIndexOf('/') + 1;
 
-        if ($icon.hasClass(expandIconClass)) {
-            $icon.removeClass(expandIconClass).addClass(collapseIconClass);
-        } else {
-            $icon.removeClass(collapseIconClass).addClass(expandIconClass);
+        if (hash) {
+            return `${location.slice(startIndex)}${hash}`;
+        } else if (location.slice(0, 1) === '/') {
+            return location.slice(startIndex);
         }
 
-        $openContentIcons.removeClass(collapseIconClass).addClass(expandIconClass);
+        return location;
     };
-
-    // remove the hash mark from a url hash
-    const trimHash = function(hash) {
-        return hash.slice(0,1);
-    }
 
     const getCurrent = function() {
         const location = window.location.pathname;
         const hash = window.location.hash;
-        const startIndex = location.lastIndexOf('/') + 1;
-        let currentPage;
-        let $target;
-        let $currentList;
-
-        if (hash) {
-            currentPage = location.slice(startIndex) + hash;
-        } else if (location.slice(0,1) === '/') {
-            currentPage = location.slice(startIndex);
-        } else {
-            currentPage = location;
-        }
-
-        $target = $container.find(`a[href="${currentPage}"]`).addClass('active');
+        const currentPage = getCurrentPage(location, hash);
+        const $target = $container.find(`a[href="${currentPage}"]`)
+            .addClass('active');
 
         if ($target.length) {
             return $target.closest('ul').parent('li');
         } else if (hash) {
-            // if the hash is not in the menu remove it and return the url
-            currentPage = location.slice(startIndex);
-            $target = $container.find(`a[href="${currentPage}"]`).addClass('active');
-
-            return $target.closest('ul').parent('li');
+            // if the hash doesn't exist in the menu remove it
+            // and use the url instead
+            return $container.find(`a[href="${location.slice(location.lastIndexOf('/') + 1)}"]`)
+                .addClass('active')
+                .closest('ul')
+                .parent('li');
         } else {
             return false;
         }
     };
 
-    const showCurrent = function($currentList) {
-        // if ($currentList) {
-            $currentList.find('ul').show();
-            $currentList.find(`.${iconClass}`).removeClass(expandIconClass).addClass(collapseIconClass);
-        // }
+    const showCurrent = function($currentList = null) {
+        if ($currentList) {
+            $currentList.find('ul.active').removeClass('active');
+            $currentList.find('ul').addClass('active');
+        }
 
-        // return;
+        return;
     };
 
     if ($container.length) {
-        const $label = $container.children('ul')
-            .children('li')
-            .has('ul')
-            .children('a');
+        const $label = $container.find(`.${labelClass}`).parent('li').has('ul').children('a');
         const $content = $label.next('ul');
-        const $expandedContent = $container.find(`.${contentHolderClass}[data-state=expanded]`);
-        let $currentList = getCurrent();
-        const $icons = $label.find(`.${iconClass}`);
+        const $currentList = getCurrent();
 
-        if (!$currentList) {
-            showDefaultContent($expandedContent, $content);
-        } else {
-            $content.hide();
-            $icons.removeClass(collapseIconClass).addClass(expandIconClass);
+        if ($currentList) {
+            $content.removeClass('active');
+            $label.removeClass('active');
             showCurrent($currentList);
         }
 
-        $(window).on('hashchange', function(e){
-            e.preventDefault();
-
+        $(window).on('hashchange', function(){
             $container.find('a.active').removeClass('active');
-            $currentList = getCurrent();
-            $content.hide();
-            $icons.removeClass(collapseIconClass).addClass(expandIconClass);
-            showCurrent($currentList);
+            $content.removeClass('active');
+            showCurrent(getCurrent());
         });
 
         $label.on('click', function(e) {
-            e.stopPropagation();
+            // e.stopPropagation();
             e.preventDefault();
 
-            const $openContent = $($content).not(':hidden');
+            const $openContent = $content.filter('.active');
+            const $openLabel = $label.filter('.active');
 
-            toggle.call(this, speed, $openContent);
-            replaceIcons.call(this, $openContent, iconClass, expandIconClass, collapseIconClass);
+            self.toggle.call(this, $openContent, $openLabel);
         });
     }
 
